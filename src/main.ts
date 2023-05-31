44c181af43807f08d8d084b8bd5eb508bc8a1eb7
flakeed <vova.krivosheya.2004@gmail.com>
2023-05-31 01:04:18 +0000

    Update export saveDeviceInfo in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 export {DEVICE_PACKAGE_NAME} from './package-name.ts'
 export {insertDevice} from './insert-device.ts'
-export {saveDeviceInfo} from './save-device-info'
+export {saveDeviceInfo} from './save-device-info.ts'
 export {getAllDeviceInfo} from './get-all-device-info'
 export {DeviceInfo} from './device-info'
 export {AllDeviceInfo} from './all-device-info'
