72995ee36fee77c0261754778b39237a5ceaaed1
flakeed <vova.krivosheya.2004@gmail.com>
2023-06-22 15:53:52 +0000

    Add getBatteryInfo to get-all-device-info.ts

--- src/get-all-device-info.ts
+++ src/get-all-device-info.ts
@@ -30,6 +30,7 @@ import { AllDeviceInfo } from "./all-device-info.js";
 export async function getAllDeviceInfo(): Promise<AllDeviceInfo> {
    return {
      ...(await Device.getInfo()),
+     ...(await Device.getBatteryInfo()),
      ...(await Device.getId()),
      ...{
        languageCode: (await Device.getLanguageCode()).value,
