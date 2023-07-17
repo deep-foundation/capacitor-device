be7b29f43fa921c458f798997012616a8507f53e
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-17 23:40:12 +0600

    Format

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
-export { Package , Package as CapacitorDevicePackage, type PackageConstructorParam, type PackageConstructorParam as CapacitorDevicePackageConstructorParam} from './package'
-export {getAllDeviceInfo} from './get-all-device-info'
-export {type DeviceInfo} from './device-info'
-export {Device, type DeviceParam} from './react-components/device'
-export {useDeviceInsertionIfDoesNotExistAndSavingData, type UseDeviceInsertionIfDoesNotExistAndSavingInfoParam,UseDeviceInsertionIfDoesNotExistAndSavingInfoResult} from './react-components/use-device-insertion-if-does-not-exist-and-saving-data'
-export {WithDeviceInsertionIfDoesNotExistAndSavingData, type WithDeviceInsertionIfDoesNotExistAndSavingDataParam} from './react-components/with-device-insertion-if-does-not-exist-and-saving-data'
-export {getDevice,type GetDeviceArg,type GetDevicecResult} from './get-device'
-export {getDeviceInsertSerialOperations, type GetDeviceInsertSerialOperationsParam} from './get-device-insert-serial-operations'
-export {getDeviceValueInsertSerialOperations, type GetDeviceValueUpdateSerialOperationsParam} from './get-device-value-update-serial-operations'
+export {
+  Package,
+  Package as CapacitorDevicePackage,
+  type PackageConstructorParam,
+  type PackageConstructorParam as CapacitorDevicePackageConstructorParam,
+} from './package';
+export { getAllDeviceInfo } from './get-all-device-info';
+export { type DeviceInfo } from './device-info';
+export { Device, type DeviceParam } from './react-components/device';
+export {
+  useDeviceInsertionIfDoesNotExistAndSavingData,
+  type UseDeviceInsertionIfDoesNotExistAndSavingInfoParam,
+  UseDeviceInsertionIfDoesNotExistAndSavingInfoResult,
+} from './react-components/use-device-insertion-if-does-not-exist-and-saving-data';
+export {
+  WithDeviceInsertionIfDoesNotExistAndSavingData,
+  type WithDeviceInsertionIfDoesNotExistAndSavingDataParam,
+} from './react-components/with-device-insertion-if-does-not-exist-and-saving-data';
+export {
+  getDevice,
+  type GetDeviceArg,
+  type GetDevicecResult,
+} from './get-device';
+export {
+  getDeviceInsertSerialOperations,
+  type GetDeviceInsertSerialOperationsParam,
+} from './get-device-insert-serial-operations';
+export {
+  getDeviceValueInsertSerialOperations,
+  type GetDeviceValueUpdateSerialOperationsParam,
+} from './get-device-value-update-serial-operations';
