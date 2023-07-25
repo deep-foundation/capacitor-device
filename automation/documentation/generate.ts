d979aee77cad35f9772598422cb11fd3cba10f76
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-25 21:54:18 +0600

    Add stdio: 'inherit' for execa

--- automation/documentation/generate.ts
+++ automation/documentation/generate.ts
@@ -2,10 +2,10 @@ import {execa} from 'execa';
 import replaceInFile from 'replace-in-file';
 
 const generate = async () => {
-  await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888']);
-  await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com']);
+  await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888'], {stdio: 'inherit'});
+  await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com'], {stdio: 'inherit'});
 
-  const { stdout: tableOfContents } = await execa('npm', ['run', '--silent','documentation:generate-table-of-contents-in-readme']);
+  const { stdout: tableOfContents } = await execa('npm', ['run', '--silent','documentation:generate-table-of-contents-in-readme'], {stdio: 'inherit'});
 
   const options = {
     files: 'README.md',
@@ -21,18 +21,18 @@ const generate = async () => {
     console.error('Error occurred:', error);
   }
 
-  await execa('git', ['add', 'README.md']);
+  await execa('git', ['add', 'README.md'], {stdio: 'inherit'});
 
-  const { exitCode } = await execa('git', ['diff', '--staged', '--quiet'], { reject: false });
+  const { exitCode } = await execa('git', ['diff', '--staged', '--quiet'], { reject: false, stdio: 'inherit' });
 
   if (exitCode === 0) {
     console.log("No changes to commit");
   } else {
-    await execa('git', ['commit', '-m', 'docs: update README.md']);
-    await execa('git', ['push', 'origin', 'main']);
+    await execa('git', ['commit', '-m', 'docs: update README.md'], {stdio: 'inherit'});
+    await execa('git', ['push', 'origin', 'main'], {stdio: 'inherit'});
   }
 
-  await execa('npm', ['run', 'library:documentation:generate']);
+  await execa('npm', ['run', 'library:documentation:generate'], {stdio: 'inherit'});
 };
 
 generate();
--- automation/library/build.ts
+++ automation/library/build.ts
@@ -1,22 +1,22 @@
 import { execa } from 'execa';
 
 const generate = async () => {
-  await execa('tsc');
+  await execa('tsc', {stdio: 'inherit'});
 
-  await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888']);
-  await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com']);
+  await execa('git', ['config', '--global', 'user.name', 'FreePhoenix888'], {stdio: 'inherit'});
+  await execa('git', ['config', '--global', 'user.email', 'freephoenix888@gmail.com'], {stdio: 'inherit'});
 
-  await execa('npm', ['run', '--silent','library:build:generate-package-class']);
+  await execa('npm', ['run', '--silent','library:build:generate-package-class'], {stdio: 'inherit'});
 
-  await execa('git', ['add', './src/package.ts']);
+  await execa('git', ['add', './src/package.ts'], {stdio: 'inherit'});
 
-  const { exitCode } = await execa('git', ['diff', '--staged', '--quiet'], { reject: false });
+  const { exitCode } = await execa('git', ['diff', '--staged', '--quiet'], { reject: false , stdio: 'inherit'});
 
   if (exitCode === 0) {
     console.log("No changes to commit");
   } else {
-    await execa('git', ['commit', '-m', 'Generate new package class']);
-    await execa('git', ['push', 'origin', 'main']);
+    await execa('git', ['commit', '-m', 'Generate new package class'], {stdio: 'inherit'});
+    await execa('git', ['push', 'origin', 'main'], {stdio: 'inherit'});
   }
 };
 
