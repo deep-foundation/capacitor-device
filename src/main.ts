63a423f997477b3c5fb52cc54c16a08cfe1b1021
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-03 21:42:13 +0600

    Add exports

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 export {
-  Package,
-  Package as CapacitorDevicePackage,
-  Package as DevicePackage,
-  type PackageOptions,
-  type PackageOptions as CapacitorDevicePackageConstructorParam,
-  type PackageOptions as DevicePackageConstructorParam,
-} from './package.js';
-export { getAllDeviceInfo } from './get-all-device-info.js';
-export { type DeviceInfo } from './device-info.js';
+  WithDeviceSync,
+  type WithDeviceSyncOptions,
+} from './react/components/with-device-sync.js';
 export {
   useDeviceSync,
   type UseDeviceInsertionIfDoesNotExistAndSavingInfoParam,
   UseDeviceInsertionIfDoesNotExistAndSavingInfoResult,
 } from './react/hooks/use-device-sync.js';
 export {
-  WithDeviceSync,
-  type WithDeviceSyncOptions,
-} from './react/components/with-device-sync.js';
+  createDeviceDecorator,
+  DeviceDecorator
+} from './create-device-decorator.js'
+export { type DeviceInfo } from './device-info.js';
+export { getAllDeviceInfo } from './get-all-device-info.js';
 export {
   getDevice,
   type GetDeviceOptions,
   type GetDevicecResult,
 } from './get-device.js';
+export {
+  insertDevice,
+  InsertDeviceOptions
+} from './insert-device.js'
 export {
   makeDeviceInsertOperations,
   type MakeDeviceInsertOperationsOptions,
@@ -30,3 +30,15 @@ export {
   makeDeviceValueUpdateOperations,
   MakeDeviceValueUpdateSerialOperationsOptions
 } from './make-device-value-update-operations.js';
+export {
+  Package,
+  Package as CapacitorDevicePackage,
+  Package as DevicePackage,
+  type PackageOptions,
+  type PackageOptions as CapacitorDevicePackageConstructorParam,
+  type PackageOptions as DevicePackageConstructorParam,
+} from './package.js';
+export {
+  updateDevice,
+  UpdateDeviceOptions
+} from './update-device.js'
