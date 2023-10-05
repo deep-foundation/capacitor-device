4a3dce930b507a91b491da22564975921e7e2fb5
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-05 19:44:17 +0600

    Add shouldAct

--- src/react/hooks/use-device-sync.ts
+++ src/react/hooks/use-device-sync.ts
@@ -17,13 +17,16 @@ export function useDeviceSync<TDeepClient extends DeepClientInstance>(
 ): UseDeviceSyncResult {
   const log = moduleLog.extend(useDeviceSync.name);
   log({ options });
-  const { deviceLinkId: deviceLinkId, deep } = options;
+  const { deviceLinkId: deviceLinkId, deep, shouldAct = true } = options;
   const [isLoading, setIsLoading] = useState<boolean | undefined>(undefined);
   log({ isLoading, setIsLoading });
   const [error, setError] = useState<unknown | undefined>(undefined);
   log({error, setError})
 
   useEffect(() => {
+    if(!shouldAct) {
+      return;
+    }
     setIsLoading(true);
     deep
       .updateDevice({
@@ -46,6 +49,7 @@ export interface UseDeviceInsertionIfDoesNotExistAndSavingInfoOptions<
    */
   deviceLinkId: number;
   deep: DeviceDecorator<TDeepClient>;
+  shouldAct?: boolean;
 }
 
 export interface UseDeviceSyncResult {
